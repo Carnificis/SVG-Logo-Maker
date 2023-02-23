b9d1967a94791ab87b304fe27b091e91a002ccfa
Jack Kelly <Carnificis@users.noreply.github.com>
2023-02-22 20:29:22 -0600

    index.js

---
+++ index.js
@@ -0,0 +1,67 @@
+const inquirer = require('inquirer');
+const {writeFile} = require('fs/promises');
+const { Triangle, Circle, Square } = require('./lib/shapes');
+const SVG = require('./lib/SVG');
+const questions = [
+  {
+    type: 'input',
+    name: 'text',
+    message: 'Enter up to three characters for the logo:',
+    validate: function(value) {
+      if (value.length > 3) {
+        return 'Text must not exceed 3 characters.';
+      }
+      return true;
+    }
+  },
+  {
+    type: 'input',
+    name: 'textColor',
+    message: 'Enter the text color (keyword or hexadecimal number):'
+  },
+  {
+    type: 'list',
+    name: 'shapeType',
+    message: 'Select a shape:',
+    choices: ['Triangle', 'Circle', 'Square']
+  },
+  {
+    type: 'input',
+    name: 'shapeColor',
+    message: 'Enter the shape color (keyword or hexadecimal number):'
+  }
+];
+
+inquirer.prompt(questions).then(({text,textColor, shapeType, shapeColor}) => {
+  let shape;
+
+  switch (shapeType) {
+    case 'Triangle':
+      shape = new Triangle();
+      break;
+    case 'Circle':
+      shape = new Circle();
+      break;
+    default:
+      shape = new Square();
+      break;
+  
+  }
+shape.setColor(shapeColor)
+const svg = new SVG()
+svg.setText(text, textColor)
+svg.setShape(shape)
+return writeFile("./examples/logo.svg", svg.render())
+
+})
+.then(() => console.log("Generated logo.svg"))
+
+.catch(err => console.log(err));
+
+    function start(){
+        inquirer.prompt(questions)
+        .then(response => {
+           // console.log(response)
+            generateMarkDown("logo.SVG",response)})
+
+}
